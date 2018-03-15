4d39c6c8569a4db67127f08277ff6da8917c7b6b
CesarHtea <chernandez_83@yahoo.com.mx>
2018-03-15 11:21:10 -0600

    First version

---
+++ src/components/Container.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Shots from './Shots';
+
+class Container extends Component {
+  render() {
+    return (
+      <header>
+        <nav className='nav'>
+          <ul className='nav__list'>
+            <li>
+              <a href='#'>All</a>
+            </li>
+            <li>
+              <a href='#'>Popular</a>
+            </li>
+            <li>
+              <a href='#'>Recent</a>
+            </li>
+            <li>
+              <a href='#'>Debuts</a>
+            </li>
+          </ul>
+        </nav>
+        <Shots />
+      </header>
+    );
+  }
+}
+
+export default Container;
---
+++ src/components/Shots.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Shot from './Shot';
+
+
+class Shots extends Component {  
+  render() {
+    return (
+      <section>
+        <div className='container'>
+          <div className='shots'>
+            <Shot />
+            <Shot />
+            <Shot />
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Shots;
